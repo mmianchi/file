41740daaa8be3ae8422e6f32a4f619e4fae9361d
mmianchi <mmianchi@gmail.com>
2018-08-21 02:27:04 -0700

    add task item and UI for delet item

--- app.js
+++ app.js
@@ -5,3 +5,36 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// 
+function loadEventListeners(){
+  //Add task event
+  form.addEventListener('submit', addTask);
+}
+
+//add Task function
+function addTask(e) {
+  if(taskInput.value === ''){
+    alert('Add a task');
+  }
+
+  //create li element
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  //create new link
+  const link = document.createElement('a');
+  link.className ='delete-item secondary-content';
+  link.innerHTML ='<i class="fa fa-remove"></i>'
+  li.appendChild(link);
+
+  taskList.appendChild(li);
+ //clear input
+  taskInput.value = '';
+  e.preventDefault();
+}
+
