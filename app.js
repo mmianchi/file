a45239a6b5e937518fbaaec9eda918c926a4ac14
mmianchi <mmianchi@gmail.com>
2018-11-04 23:55:54 -0800

    adding filter

--- app.js
+++ app.js
@@ -79,17 +79,3 @@ function filterTasks(e){
 }
 
 
-
-
-function filterTasks(e) {
-  const text = e.target.value.toLowerCase();
-
-  document.querySelectorAll('.collection-item').forEach(function (task) {
-    const item = task.firstChild.textContent;
-    if (item.toLowerCase().indexOf(text) != -1) {
-      task.style.display = 'block';
-    } else {
-      task.style.display = 'none';
-    }
-  });
-}
